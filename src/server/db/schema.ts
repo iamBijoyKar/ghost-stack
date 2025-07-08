44d665f8953062ab19c29275026156b6675f6db6
Bijoy Kar <bijoykar54321@gmail.com>
2025-07-08 22:11:08 +0530

    update: New user and stack table added

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -10,20 +10,43 @@ import { index, pgTableCreator } from "drizzle-orm/pg-core";
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator(
-  (name) => `ghost-stack-no-trpc_${name}`,
-);
+export const createTable = pgTableCreator((name) => `ghost-stack-${name}`);
 
-export const posts = createTable(
-  "post",
+export const users = createTable(
+  "user",
   (d) => ({
-    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+    id: d
+      .uuid("id")
+      .primaryKey()
+      .default(sql`gen_random_uuid()`),
+    clerkId: d.varchar({ length: 64 }).notNull().unique(),
     name: d.varchar({ length: 256 }),
+    email: d.varchar({ length: 256 }).notNull().unique(),
+    imageUrl: d.varchar({ length: 512 }).$default(() => ""),
     createdAt: d
       .timestamp({ withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
   }),
-  (t) => [index("name_idx").on(t.name)],
+  (t) => [index("user_idx").on(t.name)],
+);
+
+export const stacks = createTable(
+  "stack",
+  (d) => ({
+    id: d
+      .uuid("id")
+      .primaryKey()
+      .default(sql`gen_random_uuid()`),
+    name: d.varchar({ length: 256 }).notNull(),
+    description: d.text().$default(() => ""),
+    userId: d.uuid("user_id").notNull(),
+    createdAt: d
+      .timestamp({ withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+  }),
+  (t) => [index("stack_idx").on(t.name)],
 );
