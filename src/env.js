96b663b160078a9b5ed893adb4a82dd3af1e7b0b
Bijoy Kar <bijoykar54321@gmail.com>
2025-07-10 16:43:37 +0530

    update: Add CLERK_SECRET_KEY and CLERK_WEBHOOK_SECRET_KEY to environment schema

--- src/env.js
+++ src/env.js
@@ -11,6 +11,8 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    CLERK_SECRET_KEY: z.string(),
+    CLERK_WEBHOOK_SECRET_KEY: z.string(),
   },
 
   /**
@@ -29,6 +31,10 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    CLERK_WEBHOOK_SECRET_KEY: process.env.CLERK_WEBHOOK_SECRET_KEY,
+    // Uncomment the line below to expose a client-side variable.
+    // Note: You must also add it to the `client` schema above.
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
